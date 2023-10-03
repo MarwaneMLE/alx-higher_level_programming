e268a6eae59b6a3d66f582e02449ee84313c77c5
Marwane <khmarwane10@gmail.com>
2023-10-03 13:25:12 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-ch = 97
-while(ch < 123):
+for ch in range(97, 123):
     if chr(ch) != 'e' and chr(ch) != 'q':
-        print(chr(ch), end="")
-    ch += 1
+        print({}.format(chr(ch)), end="")
 
