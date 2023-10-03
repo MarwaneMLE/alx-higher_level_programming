324a1df62cf5874ddc0a94290b1bdf5cd69a0f02
Marwane <khmarwane10@gmail.com>
2023-10-03 14:17:50 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for i in range(25, - 1, -1):
+    c = i + ord('A')
+    if i % 2 == 1:
+        c += 32
+
+    print("{:c}".format(c), end="")
