3f0c3373a41d85e21614d439249bf3d2eeffdf14
Marwane <khmarwane10@gmail.com>
2023-10-05 17:06:24 -0700

    Python - if/else, loops, functions

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-import sys
-def main():
-    """prints the result of the addition of all arguments"""
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i+1])
-    print("{}".format(total))
-
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
 
 if __name__ == "__main__":
-    main()
+    import sys
+    add_arg(sys.argv)
