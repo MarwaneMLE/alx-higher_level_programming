38d51ab6bcbc0b32763aa8c204df690be5384327
Marwane <khmarwane10@gmail.com>
2023-10-09 10:24:27 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position"""
-    if idx >= and idx > len(my_list):
+        if idx >= and idx < len(my_list):
         my_list[idx] = element
     return my_list
