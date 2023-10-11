42ca272614eaa0e5a4e9411bb4cb5e935c60f343
Marwane <khmarwane10@gmail.com>
2023-10-11 10:30:49 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    '''replaces all occurrences of an element by another in a new list.'''
+    '''replaces all occurrences of an element in a new list.'''
     return list(map(lambda x: replace if x == search  else x, my_list))
