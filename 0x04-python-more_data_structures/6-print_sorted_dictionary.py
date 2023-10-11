0c98d5a79b47a09e984d5ee4a3122f724de5f367
Marwane <khmarwane10@gmail.com>
2023-10-11 13:20:20 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     '''Function that prints a dictionary by ordered keys.'''
-    ordered_list = sorted(list(a_dictionary.keys()))
+    ordered_list = list(a_dictionary.keys())
+    ordered_list.sort()
     for elt in ordered_list:
         print("{}: {}".format(elt, a_dictionary.get(i)))
