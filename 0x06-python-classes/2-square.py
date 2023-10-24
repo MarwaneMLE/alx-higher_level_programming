514d48e8b49cd6ee4716224aed23880ca0dfced8
Marwane <khmarwane10@gmail.com>
2023-10-24 11:59:20 -0700

    Python Classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    """ Class to count square of given number """
+    """ Class to count square of given number
+    Attribute:
+            size: size of a square (1 side).
+    """
     def __init__(self, size=0):
+        """Creates new instances of square.
+        Args:
+            size: size of the square (1 side).
+        """
         self.size =  size
 
         if not isinstance(size, int):
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    """ Class to count square of given number """
+    """ Class to count square of given number
+
+    Attributei:
+        size: size of a square (1 side).
+    """
     def __init__(self, size=0):
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
         self.__size = size
 
         if not isinstance(size,int):
@@ -9,9 +21,13 @@ class Square:
 
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         else:
             self.__size = size
 
     def area(self):
+        """Calculates the area of square.
+
+        Returns: the current square area.
+        """
         return self.__size ** 2
