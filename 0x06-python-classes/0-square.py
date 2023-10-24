ea7ee3f41d08d8774d880c0deeef2dc6d589820c
Marwane <khmarwane10@gmail.com>
2023-10-24 11:46:07 -0700

    Python Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class  Square:
     """ Class to count square of given number"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
-    """ Class that count a square of given number"""
+    """ Class that count a square of given number
+    Args:
+        size: length of a side of the square.
+    """
     def __init__(self, size):
         self.size = size
