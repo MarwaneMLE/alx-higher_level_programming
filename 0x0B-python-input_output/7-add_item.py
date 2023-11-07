14203f7e096544d4c174f794f7cda889376c2062
Marwane <khmarwane10@gmail.com>
2023-11-07 12:47:07 -0800

    Python Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Saving arguments to a list"""
 import sys
+import os.path
 
+
+args = sys.argv[1:]
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    list1 = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    list1 = []
+list1 = []
 
-for i in sys.argv[1:]:
-    list1.append(i)
-save_to_json_file(list1, "add_item.json")
+if os.path.exists("./add_item.json"):
+    list1 = load_from_json_file("add_item.json")
+
+save_to_json_file(list1 + args, "add_item.json")
